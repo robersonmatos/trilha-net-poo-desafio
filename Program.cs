f1d7d2638a24f15d5b7cd7477ba4d6c25cbf6628
Roberson de Matos <Roberson de Matos>
2025-06-17 15:59:21 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Samsung S25");
+        Smartphone nokia = new Nokia("12345678", "Samsung S25", "0123451234512345", 256);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Linkedin");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Smartphone Xiaomi 12x");
+        Smartphone iphone = new Iphone("12345678", "Xiaomi 12x", "0123451234512345", 256);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Linkedin");
+
+        Console.ReadLine(); 
+    }
+}
